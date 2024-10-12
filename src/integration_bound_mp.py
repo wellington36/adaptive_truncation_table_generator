55083465acb9060a26109815275d4d6299e425d9
Wellington36 <wellington.71319@gmail.com>
2024-10-12 01:23:12 -0300

    Fix stop condition and update gitignore

--- src/integration_bound_mp.py
+++ src/integration_bound_mp.py
@@ -6,17 +6,17 @@ def integration_bound_mp(f, g, theta, M, eps, initial_k):
     leps = log(eps)
     log_terms = [log(mpf(0))] * (M + initial_k)
 
-    if (logdiffexp(g(theta, M-1), g(theta, M)) > log(2) + leps):
+    if (g(theta, M) > log(mpf(2)) + leps):
         raise ValueError("It is not possible to reach the stopping criterion with the given M.")
 
     log_terms[k] = f(theta, k)
     k+=1
 
-    while (logdiffexp(g(theta, k-1), g(theta, k)) > log(2) + leps):
+    while (g(theta, k) > log(mpf(2)) + leps):
         log_terms[k] = f(theta, k)
         k+=1
     
     log_sum = logsumexp(log_terms)
 
-    return (k-initial_k, logsumexp([log_sum, logsumexp([g(theta, k), g(theta, k-1)]) - log(2)]))
+    return (k-initial_k, logsumexp([log_sum, logsumexp([g(theta, k), g(theta, k-1)]) - log(mpf(2))]))
 
