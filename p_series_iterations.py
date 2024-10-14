add38e76273a8cc99163a09fda99986d6710e36f
Wellington36 <wellington.71319@gmail.com>
2024-10-14 18:11:52 -0300

    Cheate table and fix some typos in the approach

--- p_series_iterations.py
+++ p_series_iterations.py
@@ -1,6 +1,7 @@
 from src.integration_bound_mp import integration_bound_mp
 from src.sequential_mp import sequential_mp
 from mpmath import mpf, log, exp, mp
+from tabulate import tabulate
 
 def f (p, n):
     return log(mpf(1)) - (p * log(mpf(n)))
@@ -9,14 +10,36 @@ def g (p, n):
     return (1 - p) * log(mpf(n)) - log(mpf(p) - 1)
 
 mp.dps = 200
-pl = [mpf(3), mpf(2), mpf('1.5'), mpf('1.4'), mpf('1.3')]
+pl = [mpf(3), mpf(2), mpf('1.6'), mpf('1.5'), mpf('1.4')]
 epsl = [mpf(2)**(mpf(-52))*mpf(10)**mpf(14), mpf(2)**(mpf(-52))*mpf(10)**mpf(12)]
-M = [10**5, 10**5, 2*10**6, 5*10**6, 10**7]
+M = [10**5, 10**5, 2*10**6, 5*10**6, 5*10**6]
+initial_k = 1
 
 
-for eps in epsl:
-    for p in pl:
-        integration = integration_bound_mp(f, g, p, M[pl.index(p)], eps, 1)
-        sequential = sequential_mp(f, p, M[pl.index(p)], eps, 1)
+terms_error_minus_2 = []
+for p in pl:
+    print(f"Evaluating: eps = {float(mpf(2)**(mpf(-52))*mpf(10)**mpf(14))} and p = {float(p)}")
 
-        print(f"eps = {eps} | p = {p} | integration = {integration[0]} | sequential = {sequential[0]}")
+    eps = mpf(2)**(mpf(-52))*mpf(10)**mpf(14)
+    _, integration = integration_bound_mp(f, g, p, M[pl.index(p)], eps, initial_k)
+    _, sequential = sequential_mp(f, p, M[pl.index(p)], eps, initial_k)
+
+    terms_error_minus_2.append((sequential, integration))
+
+terms_error_minus_4 = []
+for p in pl:
+    print(f"Evaluating: eps = {float(mpf(2)**(mpf(-52))*mpf(10)**mpf(12))} and p = {float(p)}")
+
+    eps = mpf(2)**(mpf(-52))*mpf(10)**mpf(12)
+    _, integration = integration_bound_mp(f, g, p, M[pl.index(p)], eps, initial_k)
+    _, sequential = sequential_mp(f, p, M[pl.index(p)], eps, initial_k)
+
+    terms_error_minus_4.append((sequential, integration))
+
+    data = []
+    for idx, (a, b) in enumerate(zip(terms_error_minus_2, terms_error_minus_4)):
+        data.append([f"p={pl[idx]}", a[0], a[1], b[0], b[1]])
+
+    headers = ["", "2.2x10^-2|Sequential", "2.2x10^-2|Integration", "2.2x10^-4|Sequential", "2.2x10^-4|Integration"]
+
+    print(tabulate(data, headers, tablefmt="fancy_grid"))
--- src/integration_bound_mp.py
+++ src/integration_bound_mp.py
@@ -3,20 +3,21 @@ from utils.utils import logsumexp, logdiffexp
 
 def integration_bound_mp(f, g, theta, M, eps, initial_k):
     k = initial_k
+    eps = mpf(eps)
     leps = log(eps)
     log_terms = [log(mpf(0))] * (M + initial_k)
 
-    if (g(theta, M) > log(mpf(2)) + leps):
+    if (mpf(g(theta, M)) > log(mpf(2)) + leps):
         raise ValueError("It is not possible to reach the stopping criterion with the given M.")
 
-    log_terms[k] = f(theta, k)
+    log_terms[k] = mpf(f(theta, k))
     k+=1
 
-    while (g(theta, k) > log(mpf(2)) + leps):
-        log_terms[k] = f(theta, k)
+    while (mpf(g(theta, k)) > log(mpf(2)) + leps):
+        log_terms[k] = mpf(f(theta, k))
         k+=1
     
-    log_sum = logsumexp(log_terms)
+    log_sum = logsumexp(log_terms[initial_k:(k+1)])
 
-    return (k-initial_k, logsumexp([log_sum, logsumexp([g(theta, k), g(theta, k-1)]) - log(mpf(2))]))
+    return (k-initial_k, logsumexp([log_sum, g(theta, k) - log(mpf(2)), g(theta, k-1) - log(mpf(2))]))
 
