4dea5d9375ac2072d21f7fd7ca77bbec32162fd9
Wellington36 <wellington.71319@gmail.com>
2024-10-14 15:57:35 -0300

    Add comments and improve structure of sequential approach

--- src/sequential_mp.py
+++ src/sequential_mp.py
@@ -3,35 +3,50 @@ from mpmath import mpf, log
 
 
 def sequential_mp(f, theta, M, eps, initial_k):
+    '''Approximate the sum with M terms and then evaluate iterations until reaching the error with the approximated sum.
+    
+    Parameters:
+    f                 (function): Log of the terms function
+    theta (first parameter of f): Parameter of the function
+    M                      (int): Maximum number of iterations
+    eps                  (float): Error tolerance
+    initial_k              (int): Start of the sum
+
+    Return: (iterations, approximation in log-scale)
+    '''
     k = initial_k
+    eps = mpf(eps)
     leps = log(eps)
     log_terms_brute = [log(mpf(0))] * (M+initial_k+1)
 
-    if (f(theta, M) > leps):
-        raise ValueError("It is not possible to reach the stopping criterion with the given M.")
+    # Aux function
+    def term(k):
+        return mpf(f(theta, k))
 
-    log_terms_brute[k] = f(theta, k)
-    log_terms_brute[k+1] = f(theta, k+1)
+    # Compute the approximation
+    log_terms_brute[k] = term(k)
+    log_terms_brute[k+1] = term(k+1)
     k+=1
 
     while (k < M+initial_k):
         k+=1
-        log_terms_brute[k] = f(theta, k)
+        log_terms_brute[k] = term(k)
     
     log_sum_brute = logsumexp(log_terms_brute[initial_k:(k+1)])
 
+    # Compute the terms until we reach the approximation
     k = initial_k
     log_terms = [log(mpf(0))] * (M+initial_k+1)
 
-    log_terms[k] = f(theta, k)
-    log_terms[k+1] = f(theta, k+1)
+    log_terms[k] = term(k)
+    log_terms[k+1] = term(k)
     k+=1
+    
     log_sum = logsumexp(log_terms[initial_k:(k+1)])
 
     while (logdiffexp(log_sum_brute, log_sum) >= leps):
         k+=1
-        log_terms[k] = f(theta, k)
+        log_terms[k] = term(k)
         log_sum = logsumexp([log_sum, log_terms[k]])
-    
     
     return (k-initial_k, log_sum)
